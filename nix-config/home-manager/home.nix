88784b88b36ddfe4ba5df239c31973e8996b7e0b
Ribbal Hussain <ahmedhussain32000@gmail.com>
2024-02-25 21:51:53 -0600

    Installed direnv and modified programs to be in object notation.

--- nix-config/home-manager/home.nix
+++ nix-config/home-manager/home.nix
@@ -18,35 +18,23 @@
 
   nixpkgs = {
 
-    # You can add overlays here
     overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
 
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
     ];
-    # Configure your nixpkgs instance
+
     config = {
-      # Disable if you don't want unfree packages
       allowUnfree = true;
       # Workaround for https://github.com/nix-community/home-manager/issues/2942
       allowUnfreePredicate = _: true;
     };
   };
 
-  # TODO: Set your username
   home = {
     username = "r-dev";
     homeDirectory = "/Users/r-dev";
   };
 
-  # Add stuff for your user as you see fit:
-  # programs.neovim.enable = true;
+# Packages
   home.packages = 
   with pkgs; [  
 
@@ -59,6 +47,7 @@
 	fd
 	git
 	gnumake
+	nix-direnv
 	platformio
 	ripgrep
 	starship
@@ -77,13 +66,22 @@
 
   ];
 
-  # Enable home-manager and git
-  programs.home-manager.enable = true;
-  programs.git.enable = true;
+ programs = {
+    home-manager.enable = true;
+	git.enable = true;
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+    zsh = {
+      enableCompletion = true;
+      enableAutosuggestions = true;
+      syntaxHighlighting.enable = true;
+	};
 
-  # Nicely reload system units when changing configs
-  systemd.user.startServices = "sd-switch";
+	};
 
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "23.05";
+home.stateVersion = "23.05";
+systemd.user.startServices = "sd-switch";
+
 }
