9b14606bea3f352ac701a90c19ec84c3f5fd612a
Ribbal Hussain <ahmedhussain32000@gmail.com>
2024-04-04 01:32:39 -0500

    Minor testing of hyperkey

--- nix-config/home-manager/home.nix
+++ nix-config/home-manager/home.nix
@@ -1,4 +1,3 @@
-# This is your home-manager configuration file
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
 {
   inputs,
@@ -55,7 +54,6 @@
     rustup
     tree
 		yazi
-
 
     # build tools
     cmake
