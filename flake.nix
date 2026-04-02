0fc9c90e6ab5cd4aa55432ff1c38e8ec3a46d50c
Ribbal Hussain <ribbalh0@gmail.com>
2026-04-02 14:13:10 -0500

    "Updated nightly and removed comments, everything is broken lol"

--- flake.nix
+++ flake.nix
@@ -20,21 +20,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # dms = {
-    #   url = "github:AvengeMedia/DankMaterialShell/stable";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-    #
-    # dgop = {
-    #   url = "github:AvengeMedia/dgop";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-    #
-    # dsearch = {
-    #   url = "github:AvengeMedia/danksearch";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-    #
     neovim-nightly-overlay = {
       url = "github:nix-community/neovim-nightly-overlay";
     };
